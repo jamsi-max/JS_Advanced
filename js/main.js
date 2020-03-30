1f6c34211fe920b9117628dd41f12820a86cae94
jamsi-max <jamsi@bk.ru>
2020-03-30 16:50:12 +0300

    Completed all the tasks: 1) the search is done via the drop-down menu and reactive 2) the error is displayed in the middle of the page if data from the server is not received

--- js/main.js
+++ js/main.js
@@ -1,77 +1,25 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 const app = new Vue({
-  el: '#app',
-  data: {
-    catalogUrl: '/catalogData.json',
-    products: [],
-    imgCatalog: 'https://placehold.it/200x150',
-    searchLine: '',
-    isVisibleCart: true,
-    filtrered: [],
-  },
+    el: '#app',
 
-  methods: {
-
-    getJson(url){
-      return fetch(url)
-        .then(result => result.json())
-        .catch(error => {
-          console.log(error);
-        })
-    },
-    
-    addProduct(product){
-      console.log(product.id_product);
+    // data тут больше ненужна так как все теперь в компонентах
+    data: {
+        errorLoad: '',
     },
 
-    filterGoods(){
-      this.filtrered = [];
-      const regExp = new RegExp(this.searchLine, 'i');
-      for(let product of this.products){
-        if (!regExp.test(product.product_name) ){
-          document.querySelector(`.product-item[data-id="${product.id_product}"]`).classList.add('invisible');
-        } else {
-          document.querySelector(`.product-item[data-id="${product.id_product}"]`).classList.remove('invisible');
-          this.filtrered.push(product)
-        }
-      }
-      console.log(this.filtrered)
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                    this.errorLoad = error;
+                })
+        },
     },
-
-    cartView(){
-      if (this.isVisibleCart) {
-        document.querySelector('.cart-block').classList.toggle('invisible');
-      } else {
-        document.querySelector('.cart-block').classList.toggle('invisible');
-      }
-      this.isVisibleCart = !this.isVisibleCart;   
-    },
-
-  },
-
-  computed: {
-
-    myStyle(){
-      return {display: this.visibl};
-    },
-
-  },
-
-  mounted(){
-
-    this.getJson(`${API + this.catalogUrl}`)
-      .then(data => {
-        for(let el of data){
-          this.products.push(el);
-        }
-      });
-      
-  },
-
-  // updated(){
-  //   this.products=[...this.filtred];
-  //   console.log(this.products);
-  // },
-
+    mounted() {
+        console.log(this);
+    }
 });
+
